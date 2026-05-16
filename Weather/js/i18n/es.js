d5f3510844487021a72d391622441f321e8d120c
sagiadinos <ns@smil-control.com>
2026-05-16 22:14:05 +0200

    add new languages for weather

---
+++ Weather/js/i18n/es.js
@@ -0,0 +1,80 @@
+export const es = {
+    // General
+    loading:            'Cargando datos meteorológicos…',
+    error:              'Error al cargar los datos meteorológicos.',
+    last_updated:       'Actualizado a las {{time}}',
+    refresh:            'Actualizar',
+
+    // Weather values
+    feels_like:         'Sensación térmica',
+    humidity:           'Humedad',
+    wind:               'Viento',
+    wind_direction:     'Dirección del viento',
+    forecast:           'Pronóstico',
+
+    // Units
+    unit_celsius:       '°C',
+    unit_kmh:           'km/h',
+    unit_percent:       '%',
+
+    // Weekdays (short)
+    day_0:              'Dom',
+    day_1:              'Lun',
+    day_2:              'Mar',
+    day_3:              'Mié',
+    day_4:              'Jue',
+    day_5:              'Vie',
+    day_6:              'Sáb',
+
+    // Weekdays (long)
+    day_long_0:         'Domingo',
+    day_long_1:         'Lunes',
+    day_long_2:         'Martes',
+    day_long_3:         'Miércoles',
+    day_long_4:         'Jueves',
+    day_long_5:         'Viernes',
+    day_long_6:         'Sábado',
+
+    // Months (short)
+    month_0:            'Ene',
+    month_1:            'Feb',
+    month_2:            'Mar',
+    month_3:            'Abr',
+    month_4:            'May',
+    month_5:            'Jun',
+    month_6:            'Jul',
+    month_7:            'Ago',
+    month_8:            'Sep',
+    month_9:            'Oct',
+    month_10:           'Nov',
+    month_11:           'Dic',
+
+    // Condition Keys → readable text
+    condition_sunny:                'Soleado',
+    condition_clear_night:          'Noche despejada',
+    condition_mostly_sunny:         'Mayormente soleado',
+    condition_mostly_clear_night:   'Mayormente despejado',
+    condition_partly_cloudy:        'Parcialmente nublado',
+    condition_cloudy:               'Nublado',
+    condition_fog:                  'Niebla',
+    condition_drizzle:              'Llovizna',
+    condition_freezing_drizzle:     'Llovizna gélida',
+    condition_rain:                 'Lluvia',
+    condition_freezing_rain:        'Lluvia gélida',
+    condition_showers:              'Chubascos',
+    condition_snow:                 'Nieve',
+    condition_snow_showers:         'Chubascos de nieve',
+    condition_thunderstorm:         'Tormenta',
+    condition_thunderstorm_hail:    'Tormenta con granizo',
+    condition_unknown:              'Desconocido',
+
+    // Wind directions
+    wind_N:     'N',
+    wind_NE:    'NE',
+    wind_E:     'E',
+    wind_SE:    'SE',
+    wind_S:     'S',
+    wind_SW:    'SO',
+    wind_W:     'O',
+    wind_NW:    'NO',
+};
---
+++ Weather/js/i18n/ru.js
@@ -0,0 +1,80 @@
+export const ru = {
+    // General
+    loading:            'Загрузка данных о погоде…',
+    error:              'Ошибка при загрузке данных о погоде.',
+    last_updated:       'Обновлено в {{time}}',
+    refresh:            'Обновить',
+
+    // Weather values
+    feels_like:         'Ощущается как',
+    humidity:           'Влажность',
+    wind:               'Ветер',
+    wind_direction:     'Направление ветра',
+    forecast:           'Прогноз',
+
+    // Units
+    unit_celsius:       '°C',
+    unit_kmh:           'км/ч',
+    unit_percent:       '%',
+
+    // Weekdays (short)
+    day_0:              'Вс',
+    day_1:              'Пн',
+    day_2:              'Вт',
+    day_3:              'Ср',
+    day_4:              'Чт',
+    day_5:              'Пт',
+    day_6:              'Сб',
+
+    // Weekdays (long)
+    day_long_0:         'Воскресенье',
+    day_long_1:         'Понедельник',
+    day_long_2:         'Вторник',
+    day_long_3:         'Среда',
+    day_long_4:         'Четверг',
+    day_long_5:         'Пятница',
+    day_long_6:         'Суббота',
+
+    // Months (short)
+    month_0:            'Янв',
+    month_1:            'Фев',
+    month_2:            'Мар',
+    month_3:            'Апр',
+    month_4:            'Май',
+    month_5:            'Июн',
+    month_6:            'Июл',
+    month_7:            'Авг',
+    month_8:            'Сен',
+    month_9:            'Окт',
+    month_10:           'Ноя',
+    month_11:           'Дек',
+
+    // Condition Keys → readable text
+    condition_sunny:                'Солнечно',
+    condition_clear_night:          'Ясная ночь',
+    condition_mostly_sunny:         'Преимущественно солнечно',
+    condition_mostly_clear_night:   'Преимущественно ясно',
+    condition_partly_cloudy:        'Переменная облачность',
+    condition_cloudy:               'Облачно',
+    condition_fog:                  'Туман',
+    condition_drizzle:              'Морось',
+    condition_freezing_drizzle:     'Ледяная морось',
+    condition_rain:                 'Дождь',
+    condition_freezing_rain:        'Ледяной дождь',
+    condition_showers:              'Ливни',
+    condition_snow:                 'Снег',
+    condition_snow_showers:         'Снегопад',
+    condition_thunderstorm:         'Гроза',
+    condition_thunderstorm_hail:    'Гроза с градом',
+    condition_unknown:              'Неизвестно',
+
+    // Wind directions
+    wind_N:     'С',
+    wind_NE:    'СВ',
+    wind_E:     'В',
+    wind_SE:    'ЮВ',
+    wind_S:     'Ю',
+    wind_SW:    'ЮЗ',
+    wind_W:     'З',
+    wind_NW:    'СЗ',
+};
