bb59879d1f14ac6e1990741ece4fea902b108696
sagiadinos <ns@smil-control.com>
2026-06-29 20:33:00 +0200

    add rss fetch script

---
+++ RSS-Ticker/js/RSSFetcher.js
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 sagiadinos
+'use strict';
+
+/**
+ * RSSFetcher - Handles RSS feed fetching and parsing
+ */
+export class RSSFetcher
+{
+	constructor(baseUrl = 'https://smil-control.com/samples/fetch-rss.php?feed_url=')
+	{
+		this.baseUrl = baseUrl;
+	}
+
+	fetch(feedUrl)
+	{
+		return new Promise((resolve, reject) => {
+			const requestUrl = this.baseUrl + feedUrl;
+			const request = new XMLHttpRequest();
+
+			request.open("GET", requestUrl, true);
+			request.onload = () => {
+				if (request.readyState === 4 && request.status === 200)
+					resolve(JSON.parse(request.responseText));
+				else
+					reject(new Error(request.statusText));
+			};
+			request.onerror = () => reject(new Error(request.statusText));
+			request.send(null);
+		});
+	}
+}
