80bda4b88f4dd73f7e539a5c00026cc72dbcb849
sagiadinos <ns@smil-control.com>
2026-08-24 23:21:03 +0200

    change code style from Allman to more common JS

--- RSS-Ticker/js/TickerFormater.js
+++ RSS-Ticker/js/TickerFormater.js
@@ -5,34 +5,27 @@
 /**
  * TickerFormatter - Formats RSS feed data into ticker text
  */
-export class TickerFormatter
-{
-	#tickerView = {};
-	#separator  = " +++ ";
+export class TickerFormatter {
+    #tickerView = {};
+    #separator = " +++ ";
 
-	constructor(tickerView)
-	{
-		this.#tickerView = tickerView;
-	}
+    constructor(tickerView) {
+        this.#tickerView = tickerView;
+    }
 
-	format(feedObj)
-	{
-		let tickerText = this.#separator;
+    format(feedObj) {
+        let tickerText = this.#separator;
 
-		for (let i = 0; i < feedObj.query.count; i++)
-		{
-			const itemText = feedObj.query.results.item[i].title + this.#separator;
+        for (let i = 0; i < feedObj.query.count; i++) {
+            const itemText = feedObj.query.results.item[i].title + this.#separator;
 
-			if (this.#tickerView.isNewContentSizeValid(tickerText + itemText))
-			{
-				tickerText += itemText;
-			}
-			else
-			{
-				break;
-			}
-		}
+            if (this.#tickerView.isNewContentSizeValid(tickerText + itemText)) {
+                tickerText += itemText;
+            } else {
+                break;
+            }
+        }
 
-		return tickerText;
-	}
+        return tickerText;
+    }
 }
--- RSS-Ticker/js/TickerView.js
+++ RSS-Ticker/js/TickerView.js
@@ -5,68 +5,60 @@
 /**
  * TickerCanvas - Handles all canvas rendering and animation
  */
-export class TickerView
-{
-	#canvas = document.getElementById("myTicker");
-	#ctx = {};
-	#movePixel = 1;
-	#maxCanvasWidth = 16384;
-	#x = 0;
-	#margin = 10;
-	#textWidth = 0;
-	#tickerContent = "";
-	#tickerColor = "yellow";
-	#fontSize = 30;
+export class TickerView {
+    #canvas = document.getElementById("myTicker");
+    #ctx = {};
+    #movePixel = 1;
+    #maxCanvasWidth = 16384;
+    #x = 0;
+    #margin = 10;
+    #textWidth = 0;
+    #tickerContent = "";
+    #tickerColor = "yellow";
+    #fontSize = 30;
 
-	constructor()
-	{
-		this.#ctx = this.#canvas.getContext("2d");
-	}
+    constructor() {
+        this.#ctx = this.#canvas.getContext("2d");
+    }
 
-	setStyle(color, fontSize)
-	{
-		this.#tickerColor = color;
-		this.#fontSize = parseInt(fontSize);
-	}
+    setStyle(color, fontSize) {
+        this.#tickerColor = color;
+        this.#fontSize = parseInt(fontSize);
+    }
 
-	resize()
-	{
-		this.#canvas.width = window.innerWidth;
-		this.#margin = Math.round(this.#fontSize / 3);
-		this.#canvas.height = this.#fontSize + this.#margin;
-		this.#ctx.font = `bold ${this.#fontSize}px Sans`;
-		this.#ctx.fillStyle = this.#tickerColor;
-		this.#textWidth = Math.round(this.#ctx.measureText(this.#tickerContent).width) + 1;
-		this.#x = this.#canvas.width;
-	}
+    resize() {
+        this.#canvas.width = window.innerWidth;
+        this.#margin = Math.round(this.#fontSize / 3);
+        this.#canvas.height = this.#fontSize + this.#margin;
+        this.#ctx.font = `bold ${this.#fontSize}px Sans`;
+        this.#ctx.fillStyle = this.#tickerColor;
+        this.#textWidth = Math.round(this.#ctx.measureText(this.#tickerContent).width) + 1;
+        this.#x = this.#canvas.width;
+    }
 
-	isNewContentSizeValid(text) 
-	{
-		return this.#ctx.measureText(text).width < this.#maxCanvasWidth;
-	}
+    isNewContentSizeValid(text) {
+        return this.#ctx.measureText(text).width < this.#maxCanvasWidth;
+    }
 
-	setContent(text)
-	{
-		this.#tickerContent = text;
-	}
+    setContent(text) {
+        this.#tickerContent = text;
+    }
 
-	animate()
-	{
-		this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
+    animate() {
+        this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
 
-		if (this.#x > -this.#textWidth) 
-			this.#x -= this.#movePixel;
-		else
-			this.#x = this.#canvas.width;
+        if (this.#x > -this.#textWidth)
+            this.#x -= this.#movePixel;
+        else
+            this.#x = this.#canvas.width;
 
-		this.#ctx.fillText(this.#tickerContent, this.#x, this.#canvas.height - this.#margin);
-		window.requestAnimationFrame(() => this.animate());
-	}
+        this.#ctx.fillText(this.#tickerContent, this.#x, this.#canvas.height - this.#margin);
+        window.requestAnimationFrame(() => this.animate());
+    }
 
-	start(text)
-	{
-		this.setContent(text);
-		this.resize();
-		window.requestAnimationFrame(() => this.animate());
-	}
+    start(text) {
+        this.setContent(text);
+        this.resize();
+        window.requestAnimationFrame(() => this.animate());
+    }
 }
